82e56170fa1482b76efa6475e466d1bb017f7c72
TyannaM <tjm00252019@gmail.com>
2023-02-12 17:27:32 -0600

    Added history manger form - added code to load handler event

---
+++ WebBrowser.UI/HistoryManagerForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WebBrowser.UI
+{
+    public partial class HistoryManagerForm : Form
+    {
+        public HistoryManagerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HistoryManagerForm_Load(object sender, EventArgs e)
+        {
+            var items = HistoryManager.GetItems();
+            listBox1.Items.Clear();
+
+            foreach (var item in items)
+            {
+                listBox1 = listBox1.Items.Add(string.Format("[{0} {1} {2}", item.Date, item.Title, item.URL));
+            }
+        }
+    }
+}
