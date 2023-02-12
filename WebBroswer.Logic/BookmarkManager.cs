56a3651da08b44fc6ccbff829c96c7b53373c84b
TyannaM <tjm00252019@gmail.com>
2023-02-12 16:50:53 -0600

    Completed code for add and retriving items for Boomarks and History

--- WebBroswer.Logic/BookmarkManager.cs
+++ WebBroswer.Logic/BookmarkManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebBrowser.Data.BookmarkDataSetTableAdapters;
 
 namespace WebBroswer.Logic
 {
@@ -10,7 +11,25 @@ namespace WebBroswer.Logic
     {
         public static void AddItem(BookmarkItem item)
         {
-            var
+            var adapter = new BookmarkTableAdapter();
+            adapter.Insert(item.URL, item.Title);
+        }
+
+        public static List<BookmarkItem> GetItems()
+        {
+            var adapter = new BookmarkTableAdapter();
+            var results = new List<BookmarkItem>();
+            var rows = adapter.GetData();
+
+            foreach ( var row in rows )
+            {
+                var item = new BookmarkItem();
+                item.Title = row.Title;
+                item.URL = row.URL;
+
+                results.Add(item);
+            }
+            return results;
         }
     }
 }
